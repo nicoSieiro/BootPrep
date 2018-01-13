ef1be4a403904391c5ad9d54b75cbad6cd738abd
nicoSieiro <34661163+nicoSieiro@users.noreply.github.com>
2018-01-13 18:18:42 -0300

    Add files via upload

---
+++ jsDeepDive/00-ProbandoTests/test.js
@@ -0,0 +1,11 @@
+function saludar(name) {
+    if (name) {
+        return("Hola, " + name + "!")    
+    } else {
+        return("Hola!")
+    }    
+}
+
+function sumar(n1,n2) {
+    return n1+n2
+}
